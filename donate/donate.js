5c04266f05b30e415aac1139f32246c29df8fd44
KSH67 <a64308116@gmail.com>
2024-07-17 15:05:19 +0900

    Delete donate directory

--- donate/donate.js
+++
@@ -1,6 +0,0 @@
-const copyText = document.getElementById('copyText');
-
-copyText.addEventListener('click', () => {
-    navigator.clipboard.writeText(copyText.textContent);
-    alert('복사되었습니다.');
-  });
